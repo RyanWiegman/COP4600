08e6e72beb27ba1a3ceadbc6f1b79dbfb6b314c4
RyanWiegman <ryanwiegman@gmail.com>
2021-02-06 21:10:53 -0500

    2/6

--- Hw3/mysh.c
+++ Hw3/mysh.c
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define DELIMITER " \n"
+
 char** read_line() {
     char input_string[1024];
     char* parse_string = malloc(sizeof(char));
@@ -17,47 +19,55 @@ char** read_line() {
     }
 
     fgets(input_string, 1024, stdin);
+    input_string[strlen(input_string) - 1] = '\0';
 
-    parse_string = strtok(input_string, " ");
+    parse_string = strtok(input_string, DELIMITER);
     while (parse_string != NULL) {
         args_array[index] = parse_string;
-        parse_string = strtok(NULL, " ");
+        parse_string = strtok(NULL, DELIMITER);
         index++;
     }
     
     for(int i = 0; i < index; i++){
         printf("token: %s\n", args_array[i]);
     }
-    
     return args_array;
 }
 
-void launch(char** arguments){
+int launch(char** args){
     pid_t pid;
     int status;
 
-    if(arguments == NULL){
+    if(args == NULL){
         printf("No arguments\n");
-        return;
+        return 1;
     }
 
     pid = fork();
-    if(pid == 0)
-        if(execvp(arguments[0], arguments) == -1){
+    if(pid == 0) {
+        printf("\nInside child first token: %s\n", args[0]);
+        if(execvp(args[0], args) == -1){
             printf("shell error");
             perror("shell");
         }
+    }
     else if(pid < 0){
         printf("PID error.");
         perror("PID error");
     }
     else
         while(wait(&status) != pid);
+    return 1;
 }
 
 int main() {
     char** args;
+    int run = 1;
 
-    args = read_line();
-    launch(args);
+    while(run){
+        args = read_line();
+        run = launch(args);
+        printf("run var: %d\n", run);
+    }
+   return EXIT_SUCCESS;
 }
