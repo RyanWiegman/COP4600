dc9a27c99704dc000d2f76932ce9928255bae9ec
RyanWiegman <ryanwiegman@gmail.com>
2021-02-08 20:30:04 -0500

    2/8

--- Hw3/mysh.c
+++ Hw3/mysh.c
@@ -7,11 +7,44 @@
 
 #define DELIMITER " \n"
 
+void print_cwd() {
+    char cwd[1024];
+    printf("%s\n", getcwd(cwd, sizeof(cwd)));
+}
+
+void print_history() {
+    char* line = NULL;
+    size_t buf_size = 0;
+    ssize_t line_count;
+    int counter = 0;
+    FILE* out_text = fopen("args_history.txt", "r");
+
+    line_count = getline(&line, &buf_size, out_text);
+    while (line_count >= 0){
+        printf("%d: %s",counter++, line);
+        line_count = getline(&line, &buf_size, out_text);
+    }
+}
+
+int check_builtin(char** args) {
+   
+    if(strcmp(args[0], "whereami") == 0)
+        print_cwd();
+    else if(strcmp(args[0], "history") == 0){
+        print_history();
+        return EXIT_SUCCESS;
+    } 
+    else if(strcmp(args[0], "byebye") == 0)
+        return EXIT_SUCCESS;
+    return 1;
+}
+
 char** read_line() {
     char input_string[1024];
     char* parse_string = malloc(sizeof(char));
     char** args_array = malloc(sizeof(char*));
     int index = 0;
+    FILE *intext = fopen("args_history.txt", "a");
 
     if(parse_string == NULL || args_array == NULL){
         printf("Malloc error.\n");
@@ -27,10 +60,12 @@ char** read_line() {
         parse_string = strtok(NULL, DELIMITER);
         index++;
     }
-    
+
     for(int i = 0; i < index; i++){
         printf("token: %s\n", args_array[i]);
+        fprintf(intext, "%s\n", args_array[i]);
     }
+    fclose(intext);
     return args_array;
 }
 
@@ -62,12 +97,20 @@ int launch(char** args){
 
 int main() {
     char** args;
-    int run = 1;
+    char** args_history;
+    int run = 1, clear = 1;
 
     while(run){
+        if(clear){
+            system("clear");
+            clear = 0;
+        }
+        printf("#");
+
         args = read_line();
-        run = launch(args);
-        printf("run var: %d\n", run);
+        run = check_builtin(args);
+        //run = launch(args);
+        printf("\nrun var: %d\n", run);
     }
    return EXIT_SUCCESS;
 }
