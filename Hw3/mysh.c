af8073146ccda13d97938428f9582c9479041357
RyanWiegman <ryanwiegman@gmail.com>
2021-02-08 20:53:30 -0500

    rearranged and finished history function

--- Hw3/mysh.c
+++ Hw3/mysh.c
@@ -24,6 +24,38 @@ void print_history() {
         printf("%d: %s",counter++, line);
         line_count = getline(&line, &buf_size, out_text);
     }
+    fclose(out_text);
+}
+
+void clear_history(){
+    FILE* clear = fopen("args_history.txt", "w");
+    fclose(clear);
+}
+
+int launch(char** args){
+    pid_t pid;
+    int status;
+
+    if(args == NULL){
+        printf("No arguments\n");
+        return 1;
+    }
+
+    pid = fork();
+    if(pid == 0) {
+        printf("\nInside child first token: %s\n", args[0]);
+        if(execvp(args[0], args) == -1){
+            printf("shell error");
+            perror("shell");
+        }
+    }
+    else if(pid < 0){
+        printf("PID error.");
+        perror("PID error");
+    }
+    else
+        while(wait(&status) != pid);
+    return 1;
 }
 
 int check_builtin(char** args) {
@@ -31,9 +63,12 @@ int check_builtin(char** args) {
     if(strcmp(args[0], "whereami") == 0)
         print_cwd();
     else if(strcmp(args[0], "history") == 0){
-        print_history();
-        return EXIT_SUCCESS;
-    } 
+        if(args[1] != NULL && strcmp(args[1], "-c") == 0)
+            clear_history();
+        else
+            print_history();
+        return 1;
+    }
     else if(strcmp(args[0], "byebye") == 0)
         return EXIT_SUCCESS;
     return 1;
@@ -67,32 +102,6 @@ char** read_line() {
     }
     fclose(intext);
     return args_array;
-}
-
-int launch(char** args){
-    pid_t pid;
-    int status;
-
-    if(args == NULL){
-        printf("No arguments\n");
-        return 1;
-    }
-
-    pid = fork();
-    if(pid == 0) {
-        printf("\nInside child first token: %s\n", args[0]);
-        if(execvp(args[0], args) == -1){
-            printf("shell error");
-            perror("shell");
-        }
-    }
-    else if(pid < 0){
-        printf("PID error.");
-        perror("PID error");
-    }
-    else
-        while(wait(&status) != pid);
-    return 1;
 }
 
 int main() {
